9b25b67fb3dc6df12e96ee7801557ff341e56e21
Talionidas <thierry.a.stahl@gmail.com>
2024-01-28 00:55:55 +0100

    Develop 03 Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        List<Scripture>John = new List<Scripture>;
+        Scripture scripture = new Scripture("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.", new Reference("John", 3, 16));
+        
+        while (true)
+        {
+            Console.Clear();
+            scripture.DisplayScripture();
+
+            Console.WriteLine("\nPress Enter to hide words, or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords();
+
+        }
     }
 }
