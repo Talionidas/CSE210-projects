f080ae1efdae2d0578ab39b75c30f29a88223d55
Talionidas <thierry.a.stahl@gmail.com>
2024-02-03 19:01:23 +0100

    Mindfulness Program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,47 @@
-using System;
+﻿using System.IO;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Welcome to the mindfulness program!");
+        bool run_program = true;
+
+        while(run_program) {
+            MainMenu();
+            string choice = Console.ReadLine(); // User choice
+            Console.Clear();
+
+            if(choice=="1"){
+                Console.Clear();
+                Activity breathing = new BreathingActivity();
+                breathing.intro();
+                Console.Clear();
+            }
+            else if(choice=="2"){
+                Console.Clear();
+                Activity reflecting = new ReflectingActivity();
+                reflecting.intro();
+                Console.Clear();
+            }
+            else if(choice=="3"){
+                Console.Clear();
+                Activity listing = new ListingActivity();
+                listing.intro();
+                Console.Clear();
+            }
+            else if(choice=="4"){
+                run_program = false;
+            }
+            else {
+                Console.WriteLine("Try Again.");
+            }
+        }
+    }
+    public static void MainMenu() {
+        Console.WriteLine("Please enter the numeric option below for the activity you would like to do:");
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Reflecting Activity");
+        Console.WriteLine("3. Listing Activity");
+        Console.WriteLine("4. Quit Program");
+        Console.Write("Enter your option: ");
     }
 }
