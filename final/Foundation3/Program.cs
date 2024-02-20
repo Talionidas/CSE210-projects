ab27b31d00b62804a5afd2d149876d104f4ba720
Talionidas <thierry.a.stahl@gmail.com>
2024-02-20 21:11:28 +0100

    Foundation 3+4

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address = new Address("Peterspl. 1", "Basel", "Basel Stadt", "Schweiz");
+
+        Lecture lecture = new Lecture("Die Schweiz in der Lupe'", "Eine generaue Dokumentation der Schweizer Bürger", "October 24, 2024", "7 AM", address, "Matthias Geering", 140);
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 1");
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateDetailedLecture());
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateShortLecture());
+
+        Address addressR = new Address("Rämistrasse 71", "Zürich", "Zürich", "Schweiz");
+
+        Reception reception = new Reception("The Chilean approach!", "Unsheltered Thoughts: The Chilean approach to the regulatory challenges of neurotechnological mindreading", "February 21, 2024", "1:30 PM", addressR, "Uni-events@Zürich.ch");
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 2");
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateDetailedReception());
+
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateShortReception());
+
+        Address addressOG = new Address("Dufourstrasse 50", "St. Gallen", "St. Gallen", "Schweiz");
+
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Geschichte / Militärgeschichte", "Die Schweiz im Krieg", "February 20, 2024", "9:30 PM", addressOG, "cloudy");
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 3");
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateDetailedOutdoorGathering());
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateShortOutdoorGathering());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("February 08, 2023", 30, 4.8);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("January 14, 2023", 60, 15);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("February 19, 2024", 15, 15);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
